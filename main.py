6a8684c41ce519362be60075496123d1b6e0df7a
Anirudh58 <anirudh.jan97@gmail.com>
2020-08-01 10:32:08 +0530

    code to click play/resume button

--- main.py
+++ main.py
@@ -1,28 +1,43 @@
-from PIL import ImageGrab
 import os
 import time
 import pyautogui
 import cv2
+import dlib
+from collections import deque
+import numpy as np
+
+import imutils
+from scipy.spatial import distance as dist
+from imutils.video import FileVideoStream
+from imutils.video import VideoStream
+from imutils import face_utils
+
+BUFFER_MAX = 32
+pts = deque(maxlen=BUFFER_MAX)
  
-def screenGrab():
-    # take screenshot using pyautogui 
-    image = pyautogui.screenshot() 
-       
-    # since the pyautogui takes as a  
-    # PIL(pillow) and in RGB we need to  
-    # convert it to numpy array and BGR  
-    # so we can write it to the disk 
-    image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR) 
-       
-    # writing it to the disk using opencv 
-    cv2.imwrite(os.getcwd() + '\\full_snap__' + str(int(time.time())) + '.png', image) 
+def startGame():
     
-    #box = ()
-    #im = ImageGrab.grab()
-    #im.save(os.getcwd() + '\\full_snap__' + str(int(time.time())) + '.png', 'PNG')
+    # locate the play and resume button and click them to start/resume the game
+    playButton = pyautogui.locateOnScreen('buttons/play_button.png')
+    resumeButton = pyautogui.locateOnScreen('buttons/resume_button.png')
+    
+    # Mark x and y coordinate to click
+    if playButton:
+        clickX, clickY = pyautogui.locateCenterOnScreen('buttons/play_button.png')
+    elif resumeButton:
+        clickX, clickY = pyautogui.locateCenterOnScreen('buttons/resume_button.png')
+    else:
+        print(" [INFO] GAME SCREEN NOT FOUND!")
+    
+    # perform 2 clicks, first to bring focus out of python window and other to actually 
+    # click the play/resume button
+    pyautogui.click(clickX, clickY)
+    time.sleep(2.0)
+    pyautogui.click(clickX, clickY)
+    
  
 def main():
-    screenGrab()
- 
+    startGame()
+    
 if __name__ == '__main__':
     main()
